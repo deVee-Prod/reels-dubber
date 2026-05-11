bb0eef72bfd52694a6072bfb3f888f210f43fb36
deVee Studio <davidbendavid@deVees-MacBook-Pro.local>
2026-05-12 01:46:04 +0300

    שנל העדכון

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
